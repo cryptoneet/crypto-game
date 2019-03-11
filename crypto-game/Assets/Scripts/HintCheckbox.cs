1027d82ba8468666250c54bb3c799849b402e917
Bogdan-Hasanov <47710368+Bogdan-Hasanov@users.noreply.github.com>
2019-03-11 17:38:26 +0200

    Простейший пример подсказки
    Панель с подсказкой в Семпл сцене

---
+++ crypto-game/Assets/Scripts/HintCheckbox.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintCheckbox : MonoBehaviour
+{
+    public GameObject hint;
+    public GameObject checkBox;
+    public void CheckBoxExec(bool IsActive)
+    {
+        if (IsActive) hint.SetActive(true);
+        else hint.SetActive(false);
+    }
+}
