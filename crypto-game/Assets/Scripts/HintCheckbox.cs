4665439ae9a4fe757f41834c494a2fce9b4402a7
Bogdan-Hasanov <47710368+Bogdan-Hasanov@users.noreply.github.com>
2019-03-13 18:29:07 +0200

    updated Sample scene

--- crypto-game/Assets/Scripts/HintCheckbox.cs
+++ crypto-game/Assets/Scripts/HintCheckbox.cs
@@ -6,9 +6,18 @@ public class HintCheckbox : MonoBehaviour
 {
     public GameObject hint;
     public GameObject checkBox;
+    public Animation animation1,animation2;
     public void CheckBoxExec(bool IsActive)
     {
-        if (IsActive) hint.SetActive(true);
-        else hint.SetActive(false);
+        if (IsActive)
+        {
+            hint.SetActive(true);
+            animation1.Play();
+            animation2.Play();
+        }
+        else
+        {
+            hint.SetActive(false);
+        }
     }
 }
