6cab291ef200130fcbeae1c9a92bba7b2d25120d
AndreyVoinn <andreyvoinn@gmail.com>
2019-03-13 11:11:21 +0200

    Adding audio manager for main-menu

---
+++ crypto-game/Assets/AudioManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip[] audioClips;
+    public static AudioManager instance;
+
+    private AudioSource player;
+
+    private void Awake()
+    {
+        player = GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        if (instance == null) instance = this;
+    }
+
+    public void playSound(AudioClip clip)
+    {
+        player.clip = clip;
+        player.Play();
+    }
+    public static AudioClip[] GetClips()
+    {
+        return instance.audioClips;
+    }
+    
+    public static void PlaySound(AudioClip clip)
+    {
+        instance.playSound(clip);
+    }
+}
