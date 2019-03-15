fba1ab64d89cf6959cdb5f48295893bf3684333a
AndreyVoinn <andreyvoinn@gmail.com>
2019-03-15 16:51:32 +0200

    Adding transitions (Scene manager) and pause menu

---
+++ crypto-game/Assets/Scripts/SceneTrans.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTrans : MonoBehaviour
+{
+    public Animator transitionAnim;
+    public bool startButHit;
+    public string sceneName;
+
+    public void setButHit()
+    {
+        startButHit = true;
+    }
+
+    void Update()
+    {
+        if(startButHit == true)
+        {
+            StartCoroutine(LoadScene());
+        }
+    }
+
+    IEnumerator LoadScene()
+    {
+        transitionAnim.SetTrigger("end");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
