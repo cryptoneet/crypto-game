ccc9687a5ef76e5ac9df54194bb40d370f5b7b61
Bogdan-Hasanov <47710368+Bogdan-Hasanov@users.noreply.github.com>
2019-03-25 13:52:19 +0200

    updated PopupSlideSample

--- crypto-game/Assets/Scripts/SlideTrigger.cs
+++ crypto-game/Assets/Scripts/SlideTrigger.cs
@@ -5,19 +5,36 @@ using UnityEngine;
 public class SlideTrigger : MonoBehaviour
 {
     public Animator obj;
+    public float startTime;
+    public float EndTime;
     // Start is called before the first frame update
+    bool coroutine = false;
     void Start()
     {
         obj = GetComponent<Animator>();
+        StartCoroutine(Example());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (coroutine)
         {
-           // obj.Play("Slide1move");
-            obj.GetComponent<Animator>().SetTrigger("Clicked");
+            // StartCoroutine(Example());
+            if (Input.GetMouseButtonDown(0))
+            {
+                // obj.Play("Slide1move");
+                obj.GetComponent<Animator>().SetTrigger("Clicked");
+                
+                StartCoroutine(Example());
+
+            }
         }
+    }
+    private IEnumerator Example()
+    {
+        coroutine = false;
+        yield return new WaitForSeconds(5);
+        coroutine = true;
     }
 }
