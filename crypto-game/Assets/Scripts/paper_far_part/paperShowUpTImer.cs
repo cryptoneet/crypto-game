1a297266ce3fa9632c5ea52581035c43af0b6a45
AndreyV <andreyvoinn@gmail.com>
2019-04-01 14:19:21 +0300

    Tutor part 1

---
+++ crypto-game/Assets/Scripts/paper_far_part/paperShowUpTImer.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using System.Threading;
+
+public class paperShowUpTImer : MonoBehaviour
+{
+    public Button btnState;
+    public Button btnHint;
+    //animators
+    public Animator animatorPaper;
+    public Animator hintShowAnimtr;
+
+    public float showDelay = 3f;
+    float timerHold = 4f;
+
+    public Text hintText;
+
+    bool btnClicked = false;
+    bool paperHoldState = false;
+
+    private void Start()
+    {
+           
+    }
+
+    void Update()
+    {
+        if (btnClicked)
+        {
+            showDelay -= 1 * Time.deltaTime;
+            if(showDelay < 0)
+            {
+                
+                animatorPaper.Play("paper_showup");
+                btnState.enabled = false;
+                paperHoldState = true;
+                //event destructor
+                btnClicked = false;
+            }
+        }
+
+        if (paperHoldState)
+        {
+            timerHold -= 1 * Time.deltaTime;
+
+            if(timerHold < 0)
+            {
+                hintChangeNShow();
+                paperHoldState = false;
+            }
+            
+        }
+    }
+
+    void hintChangeNShow()
+    {
+        hintText.text = "Хмм, кажется ваш коллега бросил вам листок! Что бы это могло значить? \n Давайте изучим объект поближе - тапните на него.";
+        hintShowAnimtr.GetComponent<Animator>().Play("showHint");
+        btnHint.GetComponent<Button>().onClick.AddListener(clickingAllow);
+    }
+
+    void clickingAllow()
+    {
+        btnState.enabled = true;
+    }
+
+    public void timerShowStart()
+    {
+        btnClicked = true;
+    }
+}
